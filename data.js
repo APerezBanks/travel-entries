ee139672c537a43aef05979a534c63c3c723a4a7
Alex Perez B <aperezbanks@gmail.com>
2025-01-05 23:26:27 +0100

    second commit

---
+++ data.js
@@ -0,0 +1,99 @@
+export default [
+  {
+    id: 1,
+    img: {
+      src: 'https://scrimba.com/links/travel-journal-japan-image-url',
+      alt: 'Mount Fuji',
+    },
+    title: 'Mount Fuji',
+    country: 'Japan',
+    googleMapsLink: 'https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9',
+    dates: '12 Jan, 2021 - 24 Jan, 2021',
+    text: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+  },
+  {
+    id: 2,
+    img: {
+      src: 'https://scrimba.com/links/travel-journal-australia-image-url',
+      alt: 'Sydney Opera House',
+    },
+    title: 'Sydney Opera House',
+    country: 'Australia',
+    googleMapsLink: 'https://maps.app.goo.gl/Zr17SCrsJeCEKMd36',
+    dates: '27 May, 2021 - 8 Jun, 2021',
+    text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+  },
+  {
+    id: 3,
+    img: {
+      src: 'https://scrimba.com/links/travel-journal-norway-image-url',
+      alt: 'Geirangerfjord',
+    },
+    title: 'Geirangerfjord',
+    country: 'Norway',
+    googleMapsLink: 'https://maps.app.goo.gl/fhkJuBhmFDv47tiB7',
+    dates: '01 Oct, 2021 - 18 Nov, 2021',
+    text: 'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.',
+  },
+
+  {
+    id: 4,
+    img: {
+      src: 'https://res.cloudinary.com/hello-tickets/image/upload/c_limit,f_auto,q_auto,w_1920/v1676507473/gcmtwiftzi7xfxvrt4nd.jpg',
+      alt: 'Valencia',
+    },
+    title: 'Valencia',
+    country: 'Spain',
+    googleMapsLink: 'https://maps.app.goo.gl/xmX2vDpejNfF7pBy8',
+    dates: '10 Mar, 2022 - 15 Mar, 2022',
+    text: 'Valencia is known for its City of Arts and Sciences, futuristic structures, and the annual Fallas festival.',
+  },
+  {
+    id: 5,
+    img: {
+      src: 'https://www.descubriruk.com/wp-content/uploads/2014/07/manchester.jpg',
+      alt: 'Manchester',
+    },
+    title: 'Manchester',
+    country: 'England',
+    googleMapsLink: 'https://maps.app.goo.gl/tFi3RKxnwAKP1YUx5',
+    dates: '01 Apr, 2022 - 07 Apr, 2022',
+    text: 'Manchester is a major city in the northwest of England with a rich industrial heritage and vibrant music scene.',
+  },
+  {
+    id: 6,
+    img: {
+      src: 'https://lp-cms-production.imgix.net/2022-08/poland-iStock-498559006-RFC.jpeg?fit=crop&w=3840&auto=format&q=75',
+      alt: 'Krakow',
+    },
+    title: 'Krakow',
+    country: 'Poland',
+    googleMapsLink: 'https://maps.app.goo.gl/kKfLYuA9fhUN5cNv6',
+    dates: '15 May, 2022 - 20 May, 2022',
+    text: 'Krakow, Poland’s second largest city, is known for its well-preserved medieval core and Jewish quarter.',
+  },
+  {
+    id: 7,
+    img: {
+      src: 'https://a.eu.mktgcdn.com/f/100004519/5QlR-I7B4XyrnISxsKAEEu-wWKUlrRii4yA9x1-4nlc.jpg',
+      alt: 'Budapest',
+    },
+    title: 'Budapest',
+    country: 'Hungary',
+    googleMapsLink: 'https://maps.app.goo.gl/N5FyLhzZfpRUddtu9',
+    dates: '01 Jun, 2022 - 10 Jun, 2022',
+    text: "Budapest, Hungary's capital, is bisected by the Danube River and known for its stunning architecture and thermal baths.",
+  },
+  {
+    id: 8,
+    img: {
+      src: 'https://as2.ftcdn.net/v2/jpg/01/22/35/59/1000_F_122355946_zCaLFxVA2iu1cAyLQ7SrAYZcvqkTnhmd.jpg',
+      alt: 'Berlin',
+    },
+    title: 'Berlin',
+    country: 'Germany',
+    googleMapsLink: 'https://maps.app.goo.gl/vLbCd3M3uJ2BPmLC7',
+    dates: '20 Jul, 2022 - 25 Jul, 2022',
+    text: 'Berlin, Germany’s capital, is famous for its history, vibrant arts scene, and landmarks like the Brandenburg Gate.',
+  },
+];
